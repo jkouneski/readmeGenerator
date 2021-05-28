557e4f31875e7668237c531915dd4a8aceeb614c
Joey Kouneski <jkouneski@yahoo.com>
2021-05-27 20:56:52 -0400

    index file and readme commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# readmeGenerator
-Readme Generator
+# Title
+README File Generator
+
+![badge](https://img.shields.io/static/v1?label=license&message=MIT&color=blue)
+## Description
+Node.js app which generates a README file for your project.
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Collaborators](#collaborators)
+* [Tests](#tests)
+* [Questions](#questions)
+## Installation
+Simply clone this repo and run "node index" in your terminal
+## Usage
+Input your response in the terminal for each section of the README. If the terminal returns "README Generated!" then you can check the directory you are in for the file!
+## License
+This application is covered under the MIT license.
+## Collaborators
+N/A
+## Tests
+N/A
+## Questions
+- [GitHub](https://github.com/jkouneski)
+- Email any questions to jkouneski@yahoo.com
---
+++ index.js
@@ -0,0 +1,104 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+
+// Array of questions for user input
+const inquirerPrompts = [
+  {
+    type: "name",
+    message: "Enter the title of this project",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Enter a description for this project",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Enter the steps to install this app",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Enter instructions for use of this app",
+    name: "usage",
+  },
+  {
+    type: "list",
+    message: "Choose the license for this project",
+    name: "license",
+    choices: ["Apache", "GNU", "MIT", "BSD", "Eclipse", "Mozilla", "Unlicense"],
+  },
+  {
+    type: "input",
+    message: "Enter collaborators' links to their GitHub profiles",
+    name: "collaborators",
+  },
+  {
+    type: "input",
+    message: "Enter steps to run tests if applicable",
+    name: "tests",
+  },
+
+  {
+    type: "input",
+    message: "Enter your GitHub username",
+    name: "github",
+  },
+  {
+    type: "input",
+    message: "Enter your email address",
+    name: "email",
+  },
+];
+const startGenerator = () =>
+  inquirer.prompt(inquirerPrompts).then((response) => {
+    fs.writeFile("README.md", 
+    createFile(response), 
+    (err) =>
+    err ? console.log(err) : 
+    console.log("README file generated")
+    );
+  });
+
+const createFile = ({
+  title,
+  description,
+  installation,
+  usage,
+  license,
+  collaborators,
+  tests,
+  github,
+  email,
+}) =>
+  `# Title
+${title}
+
+![badge](https://img.shields.io/static/v1?label=license&message=${license}&color=blue)
+## Description
+${description}
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Collaborators](#collaborators)
+* [Tests](#tests)
+* [Questions](#questions)
+## Installation
+${installation}
+## Usage
+${usage}
+## License
+This application is covered under the ${license} license.
+## Collaborators
+${collaborators}
+## Tests
+${tests}
+## Questions
+- [GitHub](https://github.com/${github})
+- Email any questions to ${email}
+`;
+
+startGenerator();
